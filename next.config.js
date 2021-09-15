d5c6248edb86f3c36cd001dfa22e9c97b1c156c2
PhamCongChinh <chinhpham1609@gmail.com>
2021-09-15 23:57:57 +0700

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -51,4 +51,9 @@ module.exports = {
   },
 
   productionBrowserSourceMaps: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
